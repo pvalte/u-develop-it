9056ca3195849fd8e77bb019bbb5f9832c2692b5
Paige A <pvaltemare@gmail.com>
2021-11-01 18:24:02 -0700

    add server.js and test express route

---
+++ server.js
@@ -0,0 +1,25 @@
+const express = require('express');
+
+const PORT = process.env.PORT || 3001;
+const app = express();
+
+// Express middleware
+app.use(express.urlencoded({ extended: false }));
+app.use(express.json());
+
+//get test route
+app.get('/', (req, res) => {
+    res.json({
+      message: 'Hello World'
+    });
+});
+
+// // Default response for any other request (Not Found)
+// app.use((req, res) => {
+//     res.status(404).end();
+// });
+
+//start the express.js server on port 3001
+app.listen(PORT, () => {
+    console.log(`Server running on port ${PORT}`);
+});
